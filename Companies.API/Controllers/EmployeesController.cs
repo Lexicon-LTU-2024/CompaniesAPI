02eadac98a3d42bec18225b3f7dcd9b559ec161e
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-09-04 10:58:20 +0200

    EmployeeController with Unit of work

--- Companies.API/Controllers/EmployeesController.cs
+++ Companies.API/Controllers/EmployeesController.cs
@@ -10,6 +10,8 @@ using AutoMapper;
 using Azure;
 using Microsoft.AspNetCore.JsonPatch;
 using Companies.Infrastructure.Data;
+using Domain.Contracts;
+using Companies.Infrastructure.Repository;
 
 namespace Companies.API.Controllers
 {
@@ -17,100 +19,43 @@ namespace Companies.API.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
-        private readonly DBContext _db;
+        private readonly IUnitOfWork _uow;
         private readonly IMapper _mapper;
 
-        public EmployeesController(DBContext context, IMapper mapper)
+        public EmployeesController(IUnitOfWork uow, IMapper mapper)
         {
-            _db = context;
+            _uow = uow;
             this._mapper = mapper;
         }
 
-        // GET: api/Employees
         [HttpGet]
         public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetEmployee(Guid companyId)
         {
 
-            var companyExists = await _db.Companies.AnyAsync(c => c.Id == companyId);
+            var companyExists = await _uow.Company.GetCompanyAsync(companyId, false);
 
-            if(!companyExists) return NotFound();
+            if (companyExists is null) return NotFound();
 
-            var employees = await _db.Employee.Where(e => e.CompanyId.Equals(companyId)).ToListAsync();
+            var employees = await _uow.Employee.GetEmployeesAsync(companyId, false);
 
             var employeeDtos = _mapper.Map<IEnumerable<EmployeeDto>>(employees);
 
             return Ok(employeeDtos);
         }
 
-        // GET: api/Employees/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Employee>> GetEmployee(Guid id)
-        //{
-        //    var employee = await _context.Employee.FindAsync(id);
-
-        //    if (employee == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return employee;
-        //}
-
-        //// PUT: api/Employees/5
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutEmployee(Guid id, Employee employee)
-        //{
-        //    if (id != employee.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(employee).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!EmployeeExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        //// POST: api/Employees
-        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPost]
-        //public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
-        //{
-        //    _context.Employee.Add(employee);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction("GetEmployee", new { id = employee.Id }, employee);
-        //}
-
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteEmployee(Guid companyId, Guid id)
         {
-            var companyExists = await _db.Companies
-                                    .AnyAsync(c => c.Id.Equals(companyId));
+            var companyExists = await _uow.Company.GetCompanyAsync(companyId, false);
 
-            if (!companyExists) return new NotFoundObjectResult("Company not found");
 
-            var employee = await _db.Employee.FirstOrDefaultAsync(e => e.Id.Equals(id) && e.CompanyId.Equals(companyId));
+            if (companyExists is null) return new NotFoundObjectResult("Company not found");
+
+            var employee = await _uow.Employee.GetEmployeeAsync(companyId, id, false);
             if (employee == null) return NotFound();
 
-            _db.Employee.Remove(employee);
-            await _db.SaveChangesAsync();
+            _uow.Employee.Delete(employee);
+            await _uow.CompleteAsync();
 
             return NoContent();
         }
@@ -124,7 +69,7 @@ namespace Companies.API.Controllers
 
             //if(!companyExists) return NotFound();
 
-            var employeeToPatch = await _db.Employee.FirstOrDefaultAsync(e => e.Id.Equals(id) && e.CompanyId.Equals(companyId));
+            var employeeToPatch = await _uow.Employee.GetEmployeeAsync(companyId, id, true);
             if (employeeToPatch is null) return NotFound();
 
 
@@ -136,7 +81,7 @@ namespace Companies.API.Controllers
             if (!ModelState.IsValid) return UnprocessableEntity(ModelState);
 
             _mapper.Map(dto, employeeToPatch);
-            await _db.SaveChangesAsync();
+            await _uow.CompleteAsync();
 
             return NoContent();
         }
