7c04ea37489c1b44af497e3779035ed2d7c31568
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-09-11 11:48:38 +0200

    GetComapany_IfNotAuthenticated_ShouldReturn400BadRequest()

--- Companies.Presentation/TestControllersOnlyForDemo/SimpleController.cs
+++ Companies.Presentation/TestControllersOnlyForDemo/SimpleController.cs
@@ -12,39 +12,25 @@ namespace Companies.Presentation.TestControllersOnlyForDemo;
 [ApiController]
 public class SimpleController : ControllerBase
 {
-    //private readonly IServiceManager _serviceManager;
-    //private readonly UserManager<ApplicationUser> userM;
-
-    public SimpleController(/*IServiceManager serviceManager, UserManager<ApplicationUser> userM*/)
+   
+    public SimpleController()
     {
-        //_serviceManager = serviceManager;
-        //this.userM = userM;
+      
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<CompanyDto>>> GetCompany(bool includeEmployees = false)
     {
-        //var user = User;
-        //var username = userM.GetUserName(User);
-
-        //var user1 = await userM.GetUserAsync(User);
-        //var user2 = await userM.FindByIdAsync(user1.Id);
-        //var user3 = await userM.FindByNameAsync(user1.UserName);
-        //var isInRole = await userM.IsInRoleAsync(user1, "Admin");
-
-
-        // var companyDtos = await _serviceManager.CompanyService.GetCompaniesAsync(includeEmployees);
-
-        //if (User.Identity.IsAuthenticated)
-        //{
-        //    return Ok("is auth");
-        //}
-        //else
-        //{
-        //    return Ok("is not");
-        //}
-        return Ok();
-        // return Ok(companyDtos);
+        if (User.Identity.IsAuthenticated)
+        {
+            return Ok("is auth");
+        }
+        else
+        {
+            return BadRequest("is not auth");
+        }
+      //  return Ok();
+     
     }
 
 
--- Controller.Tests/SimpleControllerTests.cs
+++ Controller.Tests/SimpleControllerTests.cs
@@ -1,5 +1,7 @@
 using Companies.Presentation.TestControllersOnlyForDemo;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 
 namespace Controller.Tests;
 
@@ -14,6 +16,32 @@ public class SimpleControllerTests
         var resultType = result.Result as OkResult;
 
         Assert.IsType<OkResult>(resultType);
-        Assert.Equal(200, resultType.StatusCode);
+        Assert.Equal(StatusCodes.Status200OK, resultType.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetComapany_IfNotAuthenticated_ShouldReturn400BadRequest() 
+    {
+        //Arrange
+        var httpContext = new Mock<HttpContext>();
+        httpContext.SetupGet(x => x.User.Identity.IsAuthenticated).Returns(false);
+
+        var controllerContext = new ControllerContext
+        {
+            HttpContext = httpContext.Object
+        };
+
+        var sut = new SimpleController();
+        sut.ControllerContext = controllerContext;
+
+        //Act
+        var res = await sut.GetCompany(false);
+        var resultType = res.Result as BadRequestObjectResult;
+
+        //Assert
+        Assert.IsType<BadRequestObjectResult>(resultType);
+        Assert.Equal(resultType.Value, "is not auth");
+
+
     }
 }
