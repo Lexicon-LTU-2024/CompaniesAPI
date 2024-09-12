125844a54b78b192fbf0fdbe59f98269a336b319
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-09-12 12:35:38 +0200

    CustomWebApplicationFactory

---
+++ IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,73 @@
+﻿using Companies.API;
+using Companies.Infrastructure.Data;
+using Domain.Models.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests;
+public  class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    public DBContext Context { get; private set; }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.ConfigureServices(services =>
+        {
+
+            var dbContextOptions = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DBContext>));
+
+            if (dbContextOptions != null)
+                services.Remove(dbContextOptions);
+
+            services.AddDbContext<DBContext>(options =>
+            {
+                options.UseInMemoryDatabase("T2");
+            });
+
+            var scope = services.BuildServiceProvider().CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<DBContext>();
+
+            //Förenkling
+            context.Companies.AddRange(
+                               [
+                                    new Company()
+                                     {
+                                         Name = "TestCompanyName",
+                                         Address = "TestAdress",
+                                         Country = "TestCountry",
+                                         Employees =
+                                             [
+                                                 new ApplicationUser
+                                                 {
+                                                     UserName = "TestUserName",
+                                                     Email = "test@test.com",
+                                                     Age = 50,
+                                                     Name = "TestName",
+                                                     Position = "TestPosition"
+                                                 }
+                                             ]
+                                     }
+                               ]);
+
+            context.SaveChanges();
+            Context = context;
+
+        });
+    }
+
+    public override ValueTask DisposeAsync()
+    {
+        Context.Database.EnsureDeleted();
+        Context.Dispose();
+        return base.DisposeAsync();
+    }
+}
--- IntegrationTests/IntegrationTests.cs
+++ IntegrationTests/IntegrationTests.cs
@@ -1,4 +1,5 @@
 using Companies.API;
+using Companies.Infrastructure.Data;
 using Companies.Shared.DTOs;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http.Json;
@@ -7,15 +8,17 @@ using System.Text.Json;
 
 namespace IntegrationTests;
 
-public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+public class IntegrationTests : IClassFixture<CustomWebApplicationFactory>
 {
     private HttpClient httpClient;
+    private DBContext context;
     private JsonSerializerOptions options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-    public IntegrationTests(WebApplicationFactory<Program> applicationFactory)
+    public IntegrationTests(CustomWebApplicationFactory applicationFactory)
     {
         applicationFactory.ClientOptions.BaseAddress = new Uri("https://localhost:5000/api/");
         httpClient = applicationFactory.CreateClient();
+        context = applicationFactory.Context;
     }
 
     [Fact]
@@ -49,5 +52,17 @@ public class IntegrationTests : IClassFixture<WebApplicationFactory<Program>>
     {
         var dto = await httpClient.GetFromJsonAsync<CompanyDto>("demo/dto");
         Assert.Equal("Working", dto?.Name);
+    } 
+    
+    [Fact]
+    public async Task Index_ShouldRetrurnExpectedMediType3()
+    {
+        var expectedCount = context.Companies.Count();
+        var dtos = await httpClient.GetFromJsonAsync<IEnumerable<CompanyDto>>("demo/getall");
+
+
+        Assert.Equal(expectedCount, dtos?.Count());
     }
+
+
 }
