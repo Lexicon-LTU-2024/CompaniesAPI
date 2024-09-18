bdf0b6bd9b61a1bf780e0ab9fe9c88426b9f4393
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-09-18 16:30:37 +0200

    Add PagedList

---
+++ Companies.Shared/Request/PagedList.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Companies.API.Paging
+{
+    public class PagedList<T> : List<T>
+    {
+
+        public MetaData MetaData { get; set; }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            MetaData = new MetaData
+                (
+                    currentPage: pageNumber,
+                    totalPages: (int)Math.Ceiling(count / (double)pageSize),
+                    pageSize: pageSize,
+                    totalCount: count
+                );
+
+            AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> CreateAsync(
+            IQueryable<T> source, int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+
+            var items = await source.Skip((pageNumber - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToListAsync();
+
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
